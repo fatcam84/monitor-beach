a487696849b3f025bb302417830eff8fd1e1a144
Cameron Gifford <48843941+fatcam84@users.noreply.github.com>
2019-09-02 02:02:42 -0400

    pi_birthday.py
    Find out if your birth date occurs in the first million digits of pi!

---
+++ pi_birthday/README.md
@@ -0,0 +1,11 @@
+This program finds out if the user's birth date occurs in the first million digits of pi.
+when run in terminal, the user is asked to put in their birth date in mmddyy format.
+
+If the 6 digit number combination occurs in the first million digits of pi, then
+the program will return a 12 digit number, the 3 numbers before birth date, 6 numbers 
+that is the birth date, and 3 numbers following the birth date.  If found, it will 
+also return the position the first digit of the month occurs as well as the last digit of the year, giving you a way to search the first million digits of pi your self if you 
+are not a beliver.
+
+If the birthdate does not occur in the first million digits of pi, then the program will
+return a sad sorry message.
---
+++ pi_birthday/pi_birthday.py
@@ -0,0 +1,22 @@
+import re
+
+if __name__ == "__main__":
+    filename = 'C:\\Users\\Camer\\Documents\\Python_Tests\\pi_million_digits.txt'
+
+    with open(filename) as file_object:
+        lines = file_object.readlines()
+
+    pi_string = ''
+    for line in lines:
+        pi_string += line.strip()
+
+    birthday = input("Enter your birthday in the form mmddyy: ")
+    if birthday in pi_string:
+        reg_birthday = 'r"' + birthday + '"'
+        print("Your birthday appears in the first million digits of pi!")
+        print("Below is the result with 3 numbers on either end of your birthday!")
+        for x in re.finditer(birthday, pi_string):
+            print(pi_string[x.start()-3:x.end()+3])
+            print(f"Found at position {x.start()} and {x.end()}.")
+    else:
+        print("Your birthday does not appear in the first million digits of pi.")
