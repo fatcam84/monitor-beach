8da589cbfdf9f0e50599e2a41c146ca99e584081
Cameron Gifford <48843941+fatcam84@users.noreply.github.com>
2019-09-02 02:13:01 -0400

    Update selectiveCopy.py

--- selectiveCopy.py
+++ selectiveCopy.py
@@ -1,3 +1,8 @@
+"""
+    This program will search a given directory for files with desired suffix and move them all to a different directory.
+    In this example, I am searching my 'Downlaods' folder and moving all the .zip files to a folder called 'zip_files'.
+"""
+
 import os, shutil
 
 os.chdir('C:\\Users\\Camer\\Downloads')
